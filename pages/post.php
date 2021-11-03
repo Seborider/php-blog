9bdd5390b9982aefd0d2bda4011cbdb3dae76da4
Seborider <mayer.sebobo@gmail.com>
2021-11-03 20:29:46 +0100

    ✨  Add post.php

---
+++ pages/post.php
@@ -0,0 +1,19 @@
+<?php include("../database.php"); ?>
+<?php include("elements/header.php"); ?>
+
+<h1>Post</h1>
+<?php
+$title = $_GET['title'];
+$post = fetch_post($title);
+ ?>
+
+ <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><?php echo $post["title"]; ?></h3>
+  </div>
+  <div class="panel-body">
+   <?php echo $post["content"]; ?>
+  </div>
+ </div>
+
+<?php include("elements/footer.php"); ?>
