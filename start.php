f85b861fdc72598d38d4c29af429a892838909f7
Seborider <mayer.sebobo@gmail.com>
2021-11-03 17:50:34 +0100

    🎉  Create start.php

---
+++ start.php
@@ -0,0 +1,21 @@
+<?php
+function autoload($className) 
+{
+    if (file_exists("./src/{$className}.php")) 
+    {
+        require "src/{$className}.php";
+    }
+}
+
+spl_autoload_register('autoload');
+
+$pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+
+$res = $pdo->query('SELECT * FROM posts');
+foreach ($res as $row) {
+    var_dump($row) ;
+}
+var_dump($res)
+
+?>
+
