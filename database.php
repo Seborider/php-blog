67bf9c7d90f171beee4e39ff93cb28d55cb21c57
Seborider <mayer.sebobo@gmail.com>
2021-11-03 20:28:27 +0100

    ✨  Add fetch post function

--- database.php
+++ database.php
@@ -5,10 +5,16 @@ $pdo = new PDO(
   ''
 );
 
-function fetchPosts() 
+function fetch_posts()
 {
-    global $pdo;
-    return $pdo->query("SELECT * FROM `posts`");
+   global $pdo;
+   return $pdo->query("SELECT * FROM `posts`");
 }
 
+function fetch_post($title)
+{
+  global $pdo;
+  $q = $pdo->query("SELECT * FROM `posts` WHERE title='{$title}'");
+  return $q->fetch();
+}
 ?>
