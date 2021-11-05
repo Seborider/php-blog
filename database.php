5113b3b62f5cd337ed4fccf63a038c51d9332572
Seborider <mayer.sebobo@gmail.com>
2021-11-05 18:08:18 +0100

    🎨  Move fetches to PostsRepository

--- database.php
+++ database.php
@@ -2,22 +2,8 @@
 $pdo = new PDO(
   'mysql:host=localhost;dbname=blog;charset=utf8',
   'blog',
-  'NEclHFPuRcZi3NLj'
+  'CZsi5loFDmmXy)Pq'
 );
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-function fetch_posts()
-{
-   global $pdo;
-   return $pdo->query("SELECT * FROM `posts`");
-}
-
-function fetch_post($id)
-{
-  global $pdo;
-  $stmt = $pdo->prepare("SELECT * FROM `posts` WHERE `id` = :id");
-  $stmt->execute(["id" => $id]);
-  return $stmt->fetch();
-
-}
 ?>
