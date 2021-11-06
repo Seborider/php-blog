67d4de0d3e48c13a626817031bed6c092472067e
Seborider <mayer.sebobo@gmail.com>
2021-11-06 15:11:51 +0100

    🎨  Implement ArrayAcces

--- pages/post.php
+++ pages/post.php
@@ -11,10 +11,10 @@ $post = $postsRepsitory->fetchPost($id);
 
  <div class="panel panel-default">
   <div class="panel-heading">
-    <h3 class="panel-title"><?php echo $post->title; ?></h3>
+    <h3 class="panel-title"><?php echo $post["title"];?></h3>
   </div>
   <div class="panel-body">
-   <?php echo nl2br($post->content); ?>
+   <?php echo nl2br($post["content"]); ?>
   </div>
  </div>
 
