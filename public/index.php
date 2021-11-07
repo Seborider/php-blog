959d2e16573d018ff67502419233948725fa4103
Seborider <mayer.sebobo@gmail.com>
2021-11-07 18:50:21 +0100

    ✨ Add public/index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . "/../init.php";
+
+$pathInfo = $_SERVER['PATH_INFO'];
+
+
+if ($pathInfo == "/index") {
+  $postsController = $container->make("postsController");
+  $postsController->index();
+} elseif ($pathInfo == "/post") {
+  $postsController = $container->make("postsController");
+  $postsController->show();
+}
+ ?>
