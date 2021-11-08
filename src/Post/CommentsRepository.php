4a63d87416b723ccf8a8bb9416e825819a56183e
Seborider <mayer.sebobo@gmail.com>
2021-11-08 12:15:00 +0100

    ✨  Add public function allByPost to fetch comments matching post_id

--- src/Post/CommentsRepository.php
+++ src/Post/CommentsRepository.php
@@ -2,20 +2,34 @@
 
 namespace App\Post;
 
+use PDO;
+
 use App\Core\AbstractRepository;
 
 class CommentsRepository extends AbstractRepository
 {
-    public function getTableName()
-    {
-    return 'comments';
-    }
-  
-    public function getModelName()
-    {
-    return 'App\\Post\\CommentModel';
-    } 
+
+  public function getTableName()
+  {
+    return "comments";
+  }
+
+  public function getModelName()
+  {
+    return "App\\Post\\CommentModel";
+  }
+
+  public function allByPost($id)
+  {
+    $table = $this->getTableName();
+    $model = $this->getModelName();
+
+    $stmt = $this->pdo->prepare("SELECT * FROM `$table` WHERE post_id = :id");
+    $stmt->execute(['id' => $id]);
+
+    $comments = $stmt->fetchAll(PDO::FETCH_CLASS, $model);
+
+    return $comments;
+  }
 }
-
-
-?>
+ ?>
