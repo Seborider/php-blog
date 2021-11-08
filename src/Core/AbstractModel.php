29d57560623931997b11ae63d4c6a1103d1425a2
Seborider <mayer.sebobo@gmail.com>
2021-11-08 12:11:50 +0100

    🍻  Whitespace wierdness

--- src/Core/AbstractModel.php
+++ src/Core/AbstractModel.php
@@ -6,22 +6,22 @@ use ArrayAccess;
 
 abstract class AbstractModel implements ArrayAccess
 {
-    public function offsetExists($offset)
-    {
-        return isset($this->$offset);
-    }
-    public function offsetGet($offset)
-    {
-        return $this->$offset;
-    }
-    public function offsetSet($offset, $value)
-    {
-        $this->$offset = $value;
-    }
-    public function offsetUnset($offset)
-    {
-        unset($this->$offset);
-    }
+  public function offsetExists($offset)
+  {
+    return isset($this->$offset);
+  }
+  public function offsetGet($offset)
+  {
+    return $this->$offset;
+  }
+  public function offsetSet($offset, $value)
+  {
+    $this->$offset = $value;
+  }
+  public function offsetUnset($offset)
+  {
+    unset($this->$offset);
+  }
 }
 
-?>
+ ?>
