de51617cf1550d71c2acf3e44c6aaccd0c2921b3
Seborider <mayer.sebobo@gmail.com>
2021-11-08 12:15:31 +0100

    ⚰️  Remove Whitespaces

--- src/Post/CommentsRepository.php
+++ src/Post/CommentsRepository.php
@@ -3,22 +3,18 @@
 namespace App\Post;
 
 use PDO;
-
 use App\Core\AbstractRepository;
 
 class CommentsRepository extends AbstractRepository
 {
-
   public function getTableName()
   {
     return "comments";
   }
-
   public function getModelName()
   {
     return "App\\Post\\CommentModel";
   }
-
   public function allByPost($id)
   {
     $table = $this->getTableName();
