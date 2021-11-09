ad26e0f468515d72dbfb1535e4d841c7f8c8ebf9
Seborider <mayer.sebobo@gmail.com>
2021-11-09 16:25:41 +0100

    💄  Add edit button

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -6,6 +6,8 @@
 <ul>
     <a href="logout" > 
         <input type="submit" value="Ausloggen" class="btn btn-primary" />   
+    <a href="posts-admin" > 
+        <input type="submit" value="Edit Posts" class="btn btn-primary" />   
 </ul>
 
 <?php require __DIR__ . "/../layout/footer.php"; ?>
