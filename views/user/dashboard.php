4003460eb051c57573820f9bf39fee01450eabd8
Seborider <mayer.sebobo@gmail.com>
2021-11-09 14:45:56 +0100

    💄  Add dashboard page

---
+++ views/user/dashboard.php
@@ -0,0 +1,11 @@
+<?php require __DIR__ . "/../layout/header.php"; ?>
+
+<br />
+<br />
+
+<ul>
+    <a href="logout" > 
+        <input type="submit" value="Ausloggen" class="btn btn-primary" />   
+</ul>
+
+<?php require __DIR__ . "/../layout/footer.php"; ?>
