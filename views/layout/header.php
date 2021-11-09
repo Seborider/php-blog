1990b96bd349b086ce412c0cfa3e1171b855081b
Seborider <mayer.sebobo@gmail.com>
2021-11-09 16:53:29 +0100

    💄  Add link to login page in header nav

--- views/layout/header.php
+++ views/layout/header.php
@@ -40,6 +40,7 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="index">Home</a></li>
+            <li class="active"><a href="login">Login</a></li>
           </ul>
         </div>
       </div>
